38cd9da8dfea206fd77ba55675e2a8e82a4cb010
DELL <DELL@DESKTOP-H1G8A5L>
2019-02-14 15:25:38 +0200

    Add project

---
+++ MyProject/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		int y = s.nextInt();
+		System.out.println(x+y);
+	}
+}
